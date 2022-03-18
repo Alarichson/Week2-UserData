2b6b22d4e5cae40b7b6406db1c3c6f330ca5090f
Kasutaja Kasutaja <ajurgens@tktk.ee>
2022-03-18 14:05:48 +0200

    Control Flow

---
+++ DrivingLisence/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DrivingLisence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sünniaastat;
+            //programm arvutab kasutaja vanust
+            //kui kasutaja on noorem kui 18, siis programm kuvab konsoolis
+            //"Oled liiga noor, et juhilube saada";
+            //kui kasutaja on piisavalt vana, et juhilube saada";
+            //kui kasutaja on 18, siis programm kuvab konsoolis
+            //"Palju õnne! Nüüd saad juhilube taotleda"
+
+            Console.WriteLine("Mis on su sünniaasta?");
+            int YearOfBirth = Convert.ToInt32(Console.ReadLine());
+            int UserAge = 2022 - YearOfBirth;
+
+            if (UserAge > 18)
+            {
+                Console.WriteLine("Oled piisavalt vana, et juhilube taotleda");
+            }
+            else if (UserAge < 18)
+            {
+                Console.WriteLine("Oled liiga noor, et juhilube saada");
+            }
+            else
+            {
+                Console.WriteLine("Palju õnne! Nüüd saad juhilube taotleda.");
+            }
+            Console.WriteLine("Kena päeva!");
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ UserAge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust;
+            //programm arvutab kasutaja sünniaastat;
+            //programm kuvab sünniaastat konsoolis;
+            //"oled sündinud aastal {YearOfBirth}";
+
+            Console.WriteLine("Mis on su sünniaasta?");
+            int YearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int UserAge = 2022 - YearOfBirth;
+
+            Console.WriteLine($"Sa oled {UserAge} aastat vana.");
+
+            Console.ReadLine();
+        }
+    }
+}
